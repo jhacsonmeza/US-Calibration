81d3dcb662c68b8a6d41cc011c7c19530f30615a
jhacsonmeza <jhacsonmeza121@gmail.com>
2019-05-04 13:46:19 -0500

    Matlab codes replaced by Python equivalent codes
    Matlab codes Crop_USimages.m (to crop the acquired US images) and
    Get_crossPoint.m (to select the cross-wire image coordinates) were
    replaced by the same code implemented with Python.

---
+++ crop_USimages.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 19:46:11 2019
+
+@author: Jhacson Meza
+"""
+
+import os
+import cv2
+import glob
+
+
+base = 'Calibration test 30-04-19/'
+
+
+
+I = sorted(glob.glob(base+'acquisitionUS/US/*.jpg'), key=os.path.getmtime)
+
+storage_path = base+'acquisitionUS/UScrop'
+
+if not os.path.exists(storage_path):
+    os.makedirs(storage_path)
+
+for imn in I:
+    image_name = os.path.basename(imn).split('.')
+    
+    im = cv2.imread(imn, 0)
+    imc = im[67:67+400,293:293+230]
+    
+    cv2.imwrite('{}/{}.{}'.format(storage_path, image_name[0], 
+                image_name[1]), imc)
---
+++ crossPoint.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 13:10:26 2019
+
+@author: Jhacson Meza
+"""
+
+import os
+import cv2
+import glob
+import numpy as np
+import scipy.io as sio
+from matplotlib import pyplot as plt
+
+
+base = 'Calibration test 30-04-19/'
+
+
+I = sorted(glob.glob(base+'acquisitionUS/UScrop/*.jpg'), key=os.path.getmtime)
+
+pts =[]
+T_P_W = []
+for imn in I:
+    im = cv2.imread(imn, 0)
+    
+    plt.figure('Image {}'.format(os.path.basename(imn)))
+    plt.axis('off'), plt.imshow(im)
+    plt.show()
+    p = plt.ginput(1,mouse_add=3,mouse_pop=1,mouse_stop=2,timeout=0)
+    plt.close()
+    
+    pts.append(p[0])
+
+pts = np.array(pts)
+
+sio.savemat(base+'crossP.mat', {'crossP':pts})
