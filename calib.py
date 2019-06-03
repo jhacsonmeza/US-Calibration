300c0d0cebafb030c7ad66c369270d57698918f9
jhacsonmeza <jhacsonmeza121@gmail.com>
2019-06-03 09:22:34 -0500

    Reconstruction Precision estimation (one calib)
    Calibration precision estimation before was measured by reconstructing the
    cross-wire point in the world frame. Now, following the literature, we
    estimate the reconstruction precision (RP) denoted in literature as
    \mu_{RP}. This precision metric is calculated with a single dataset, that is,
    using only one calibration.

--- calib.py
+++ calib.py
@@ -5,7 +5,7 @@ import scipy.io as sio
 
 
 
-base = 'Calibration test 21-05-19 part1/'
+base = 'Calibration test 19-05-31/'
 
 
 # load known variables
@@ -15,24 +15,67 @@ with open(base+'probe_pose.pkl','rb') as file:
 pts = sio.loadmat(base+'crossP.mat')['crossP']
 
 
-# Calibration object
+# Create calibration object
 calib = calibration.Calibration(pts, T_P_W)
-
-
 
 # Model construction
 f, J = calib.model()
+
+# Equations and Jacobian matrix construction
 eq, Jeq = calib.calibEquations(f, J)
+
+# Calibrate with iterative Levenberg-Marquardt algorithm
 x = calib.iterativeCalibraion(eq, Jeq)
 
+# Get results
 sx, sy = x[9], x[10]
 T_I_P = calib.T(x[3], x[4], x[5], x[6], x[7], x[8])
 T_W_C = calib.T(x[0], x[1], x[2], 0, 0, 0)
 
-err = []
+# Estimate reconstruction precision (RP) named in literature as μ_RP:
+rec = []
 for i in range(len(pts)):
-    phat = T_W_C @ T_P_W[i] @ T_I_P @ np.array([sx*pts[i,0],sy*pts[i,1],0,1])
+    phat = T_P_W[i] @ T_I_P @ np.array([sx*pts[i,0],sy*pts[i,1],0,1])
     
-    err.append(np.linalg.norm(phat[:3]))
+    rec.append(phat)
 
-print('\nPrecision = {} mm'.format(sum(err)/len(err)))
+rec = np.array(rec)
+err = np.linalg.norm(rec-rec.mean(0),axis=1)
+mu_RP = err.mean()
+print('\n\u03BC_RP = {} mm, with {} images'.format(mu_RP, pts.shape[0]))
+
+
+# Filter out points with high reconstruction error
+ind = err < 0.5
+
+pts2 = pts[ind]
+T_P_W2 = []
+for i in range(len(err)):
+    if ind[i]:
+        T_P_W2.append(T_P_W[i])
+        
+# Replace data in the object
+calib.setData(pts2, T_P_W2)
+
+# Equations and Jacobian matrix construction
+eq, Jeq = calib.calibEquations(f, J)
+
+# Calibrate with iterative Levenberg-Marquardt algorithm
+x = calib.iterativeCalibraion(eq, Jeq)
+
+# Get results
+sx, sy = x[9], x[10]
+T_I_P = calib.T(x[3], x[4], x[5], x[6], x[7], x[8])
+T_W_C = calib.T(x[0], x[1], x[2], 0, 0, 0)
+
+# Estimate reconstruction precision (RP) named in literature as μ_RP:
+rec2 = []
+for i in range(len(pts2)):
+    phat = T_P_W2[i] @ T_I_P @ np.array([sx*pts2[i,0],sy*pts2[i,1],0,1])
+    
+    rec2.append(phat)
+
+rec2 = np.array(rec2)
+err2 = np.linalg.norm(rec2-rec2.mean(0),axis=1)
+mu_RP2 = err2.mean()
+print('\n\u03BC_RP = {} mm, with {} images'.format(mu_RP2, pts2.shape[0]))
