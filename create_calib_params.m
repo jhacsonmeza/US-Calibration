7b7e8c18aaacc68f460312097dc85b2c2144034d
jhacsonmeza <jhacsonmeza121@gmail.com>
2019-07-06 15:08:08 -0500

    Save fundamental matrix in the calib paramaters

--- create_calib_params.m
+++ create_calib_params.m
@@ -1,5 +1,5 @@
 clear, clc
-root = 'Calibration test 08-05-19\';
+root = 'Calibration test 19-06-08/part1/';
 
 load([root,'stereoParams.mat'])
 
@@ -7,6 +7,7 @@ K1 = stereoParams.CameraParameters1.IntrinsicMatrix';
 K2 = stereoParams.CameraParameters2.IntrinsicMatrix';
 R = stereoParams.RotationOfCamera2';
 t = stereoParams.TranslationOfCamera2';
+F = stereoParams.FundamentalMatrix;
 
 distCoeffs1 = [stereoParams.CameraParameters1.RadialDistortion(1:2) ...
     stereoParams.CameraParameters1.TangentialDistortion ...
@@ -16,4 +17,4 @@ distCoeffs2 = [stereoParams.CameraParameters2.RadialDistortion(1:2) ...
     stereoParams.CameraParameters2.TangentialDistortion ...
     stereoParams.CameraParameters2.RadialDistortion(3)];
 
-save([root,'Params.mat'],'K1','K2','R','t','distCoeffs1','distCoeffs2')
+save([root,'Params.mat'],'K1','K2','R','t','F','distCoeffs1','distCoeffs2')
