93db8d0fe292e1c60b70822335ae3fab9fab78d5
Jhacson Meza <jhacsonmeza121@gmail.com>
2019-09-12 12:50:43 -0500

    Crop US scans with 5cm dpth

--- cropUSimgs.py
+++ cropUSimgs.py
@@ -10,13 +10,13 @@ import cv2
 import glob
 
 
-base = 'Calibration test 21-05-19 part1/'
+base = os.path.relpath('Calibration datasets/Calibration test 19-09-12/data2')
 
 
 
-I = sorted(glob.glob(base+'acquisitionUS/US/*.bmp'), key=os.path.getmtime)
+I = sorted(glob.glob(os.path.join(base,'US','*.bmp')), key=os.path.getmtime)
 
-storage_path = base+'acquisitionUS/UScrop'
+storage_path = os.path.join(base,'UScrop')
 
 if not os.path.exists(storage_path):
     os.makedirs(storage_path)
@@ -25,7 +25,8 @@ for imn in I:
     image_name = os.path.basename(imn).split('.')
     
     im = cv2.imread(imn, 0)
-    imc = im[67:67+400,293:293+230]
+#    imc = im[67:67+400,293:293+230] # 7cm depth
+    imc = im[67:67+409,247:247+322] # 5cm depth
     
     cv2.imwrite('{}/{}.{}'.format(storage_path, image_name[0], 
                 image_name[1]), imc)
